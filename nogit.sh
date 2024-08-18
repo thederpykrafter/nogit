f3327829e828b48cd62a79f18413d656c6ae5b59
thederpykrafter <thederpykrafter@gmail.com>
2024-08-17 21:09:12 -0400

    find all and read from ignore file properly

--- nogit.sh
+++ nogit.sh
@@ -15,13 +15,13 @@ for dir in $check_dirs; do
   fi
 done
 
-all_proj=$(<~/Dev/sh/nogit/tmp.txt)
+all_proj=$(cat ~/Dev/sh/nogit/tmp.txt)
 
 
 for proj in $all_proj; do
-  if [ ! -d ~/$proj/.git ]; then
-    if fd --maxdepth 1 --full-path --type f . ~/$proj | grep "/" &> /dev/null; then
-      if ! cat ~/Dev/sh/nogit/ignore | grep $dir; then
+  if [ ! -d ~/$proj/.git ]; then # if has .git
+    if fd --maxdepth 1 --full-path --type f . ~/$proj | grep "/" &> /dev/null; then # if files in dir
+      if ! cat ~/Dev/sh/nogit/ignore | grep $proj &> /dev/null; then
         echo $proj
       fi
     fi
