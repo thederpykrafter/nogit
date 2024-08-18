1ddf59cb448ac0d46e2e70c1482be579b4e5bcd3
thederpykrafter <thederpykrafter@gmail.com>
2024-08-18 16:33:56 -0400

    clean up even more

--- nogit.sh
+++ nogit.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 prev=$PWD
 
-function findDirs() {
-  cd ~
-  fd . $1 --full-path --maxdepth $2 --type d
-}
-
-
 function nogit() {
-  for x in $(findDirs $1 $2); do
+  cd ~
+  for x in $(fd . $1 --full-path --maxdepth $2 --type d); do
     cd ~/$x
-
       if ! cat ~/Dev/sh/nogit/ignore | grep $x &> /dev/null; then
         [[ ! -d .git ]] && \
         [[ $(fd --maxdepth 1 --type f) != "" ]] && echo $x # check for files
       fi
-
   done
 }
 
