65551b68fbbdda98f1f3a73f5946c14834292931
thederpykrafter <thederpykrafter@gmail.com>
2025-05-29 20:12:56 -0400

    check if output is being piped

--- nogit.sh
+++ nogit.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 prev=$PWD
 
-# NOTE: ignores dirs with no files
+# [[ -t 1 ]] && \
+  #   echo "nogit is attached to TTY"
+#
+# [[ -p /dev/stdout ]] && \
+  #   echo "nogit is attached to a pipe" >&2
+#
+# [[ ! -t 1 && ! -p /dev/stdout ]] && \
+  #   echo "nogit is attached to a redirection" >&2
+
+#NOTE: ignores dirs with no files
 function nogit() {
-	cd ~
-	echo -e "\x1b[94m"$1"\x1b[m"
-	IFS=$'\n'; for dir in $(fd . $1 --full-path --maxdepth $2 --type d); do
-		cd ~/$dir
-		if ! cat ~/Dev/sh/nogit/ignore | grep $dir &> /dev/null; then
-			[[ ! -d .git ]] && \
-				[[ $(fd --maxdepth 1 --type f) != "" ]] && \
-				echo $dir # check for files
-		fi
-	done
+  cd ~
+  [[ -t 1 && ! -p /dev/stdout ]] && \
+    echo -e "\x1b[94m"$1"\x1b[m" >&2
+
+  IFS=$'\n'; for dir in $(fd . $1 --full-path --maxdepth $2 --type d); do
+    cd ~/$dir
+    if ! cat ~/Dev/sh/nogit/ignore | grep $dir &> /dev/null; then
+      [[ ! -d .git ]] && \
+        [[ $(fd --maxdepth 1 --type f) != "" ]] && \
+        echo "~/$dir" # check for files
+    fi
+  done
 }
 
 # Usage:
