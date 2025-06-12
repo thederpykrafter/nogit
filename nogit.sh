e4b5dea65961a2d1df723ca51cd44206ee532bb0
thederpykrafter <thederpykrafter@gmail.com>
2025-06-11 22:31:32 -0400

    update nogit & ignore endeavour configs

--- nogit.sh
+++ nogit.sh
@@ -13,25 +13,42 @@ prev=$PWD
 #NOTE: ignores dirs with no files
 function nogit() {
   cd ~
+  # only print this if not being piped or redirected
   [[ -t 1 && ! -p /dev/stdout ]] && \
     echo -e "\x1b[94m"$1"\x1b[m" >&2
 
   IFS=$'\n'; for dir in $(fd . $1 --full-path --maxdepth $2 --type d); do
     cd ~/$dir
+    # ignore paths in $BASH_SOURCE/ignore
     if ! cat ~/Dev/sh/nogit/ignore | grep $dir &> /dev/null; then
-      [[ ! -d .git ]] && \
-        [[ $(fd --maxdepth 1 --type f) != "" ]] && \
-        echo "~/$dir" # check for files
+      # check for files
+      [[ ! -d .git ]] && [[ $(fd --maxdepth 1 --type f) != "" ]] && \
+        echo "~/$dir"
     fi
   done
 }
 
-# Usage:
-# nogit [dir] [depth]
-nogit Dev 2
-nogit .config 1
-# check if ~/Documents exists for termux compatibility
-[[ -d ~/Assets ]] && nogit Assets 2
-[[ -d ~/Documents ]] && nogit Documents 1
+function main_nogit() {
+  # Usage:
+  # nogit [dir] [depth]
+  nogit Dev 2
+  nogit .config 1
+  # check if dirs exists for termux compatibility
+  [[ -d ~/Assets ]] && nogit Assets 2
+  [[ -d ~/Documents ]] && nogit Documents 1
+}
 
+#TODO: dont overite entire ignore everytime
+function should_ignore() {
+  read -n1 -sp "Select paths to ignore? [y/N]" answer
+  if [[ $answer == "y" ]] || [[ $answer = "Y" ]]; then
+    SRC_DIR=`dirname "$0"`
+    [[ $SRC_DIR == "." ]] && SRC_DIR=$(pwd)
+    main_nogit | fzf | tee $SRC_DIR/ignore > /dev/null
+  fi
+  echo
+}
+
+main_nogit
+[[ -t 1 && ! -p /dev/stdout ]] && [[ `main_nogit` != "" ]] && should_ignore
 cd $prev
