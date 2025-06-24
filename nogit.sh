bfd435488ee56506a715d6e93ed2c3b6792435ce
thederpykrafter <thederpykrafter@gmail.com>
2025-06-23 22:27:12 -0400

    formatting

--- nogit.sh
+++ nogit.sh
@@ -2,28 +2,29 @@
 prev=$PWD
 
 # [[ -t 1 ]] && \
-  #   echo "nogit is attached to TTY"
+#   echo "nogit is attached to TTY"
 #
 # [[ -p /dev/stdout ]] && \
-  #   echo "nogit is attached to a pipe" >&2
+#   echo "nogit is attached to a pipe" >&2
 #
 # [[ ! -t 1 && ! -p /dev/stdout ]] && \
-  #   echo "nogit is attached to a redirection" >&2
+#   echo "nogit is attached to a redirection" >&2
 
 #NOTE: ignores dirs with no files
 function nogit() {
   cd ~
   # only print this if not being piped or redirected
-  [[ -t 1 && ! -p /dev/stdout ]] && \
-    echo -e "\x1b[94m"$1"\x1b[m" >&2
+  [[ -t 1 && ! -p /dev/stdout ]] \
+    && echo -e "\x1b[94m"$1"\x1b[m" >&2
 
-  IFS=$'\n'; for dir in $(fd . $1 --full-path --maxdepth $2 --type d); do
+  IFS=$'\n'
+  for dir in $(fd . $1 --full-path --maxdepth $2 --type d); do
     cd ~/$dir
     # ignore paths in $BASH_SOURCE/ignore
     if ! cat ~/Dev/sh/nogit/ignore | grep $dir &> /dev/null; then
       # check for files
-      [[ ! -d .git ]] && [[ $(fd --maxdepth 1 --type f) != "" ]] && \
-        echo "~/$dir"
+      [[ ! -d .git ]] && [[ $(fd --maxdepth 1 --type f) != "" ]] \
+        && echo "~/$dir"
     fi
   done
 }
@@ -42,7 +43,7 @@ function main_nogit() {
 function should_ignore() {
   read -n1 -sp "Select paths to ignore? [y/N]" answer
   if [[ $answer == "y" ]] || [[ $answer = "Y" ]]; then
-    SRC_DIR=`dirname "$0"`
+    SRC_DIR=$(dirname "$0")
     [[ $SRC_DIR == "." ]] && SRC_DIR=$(pwd)
     main_nogit | fzf | tee $SRC_DIR/ignore > /dev/null
     sort -o $SRC_DIR/ignore $SRC_DIR/ignore
@@ -51,5 +52,5 @@ function should_ignore() {
 }
 
 main_nogit
-[[ -t 1 && ! -p /dev/stdout ]] && [[ `main_nogit` != "" ]] && should_ignore
+[[ -t 1 && ! -p /dev/stdout ]] && [[ $(main_nogit) != "" ]] && should_ignore
 cd $prev
